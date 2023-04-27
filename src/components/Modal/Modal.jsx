a89ef7aa54e6e52f24fcd0906e11fcc1d9285f0a
Zak Franczak <zakfranczak@gmail.com>
2023-04-27 08:43:18 -0500

    added some responsiveness to the project section

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Modal.css'
 
 const Modal = () => {
+  const [modal, setModal] = useState(false)
+
+  const toggleModal = () => {
+    setModal(!modal)
+  }
+
+  if (modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
   return (
-    <div className='modal'>
-      <div onClick={toggleModal} className='overlay'></div>
-      <div className='modal-content'>
-        <h2>Hello Modal</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          numquam modi molestias dolores? Veniam dicta eveniet id totam qui
-          aspernatur nulla, quos exercitationem magni tempora, voluptatum ipsa.
-          Voluptates, saepe quibusdam.
-        </p>
-        <button className='close-modal' onClick={toggleModal}>
-          CLOSE
-        </button>
-      </div>
-    </div>
+    <>
+      <button onClick={toggleModal} className='btn-modal'>
+        Open
+      </button>
+
+      {modal && (
+        <div className='modal'>
+          <div onClick={toggleModal} className='overlay'></div>
+          <div className='modal-content'>
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium numquam modi molestias dolores? Veniam dicta eveniet
+              id totam qui aspernatur nulla, quos exercitationem magni tempora,
+              voluptatum ipsa. Voluptates, saepe quibusdam.
+            </p>
+            <button className='close-modal' onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
