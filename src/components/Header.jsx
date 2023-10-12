5fa42bca1e7b598b0c9b2759746b957bc0da5c05
Zak Franczak <zakfranczak@gmail.com>
2023-10-12 10:01:08 -0500

    Updated about me and added a new project.  Changed hero background and project layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import '../styles/Header.css'
-import headshot from '../assets/zak_headshot_in_circle.png'
-import background from '../assets/explore.jpg'
-import handleScrollToSection from './ScrollToSection.jsx'
-import { FaChevronDown } from 'react-icons/fa'
+import '../styles/Header.css';
+import headshot from '../assets/zak_headshot_in_circle.png';
+import background from '../assets/green-mist.jpg';
+import handleScrollToSection from './ScrollToSection.jsx';
+import { FaChevronDown } from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -10,7 +10,8 @@ const Header = () => {
       className='header'
       id='header'
       style={{
-        backgroundImage: `url(${background})`,
+        // backgroundImage: `url(${background})`,
+        backgroundColor: '#13293D',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
@@ -22,23 +23,27 @@ const Header = () => {
           <h1 className='title'>Zachary Franczak</h1>
           <h2 className='sub-title'>Front End Developer</h2>
           <p className='p'>
-            Hi, I'm Zak, a front-end web developer with a passion for creating
-            captivating web experiences. With a degree in Digital Media: Game
-            Development and a professional background in theatre and music, I
-            bring a unique perspective to my web development work.
+            Hello! I'm Zachary, a front-end developer with a unique perspective
+            and versatile skill set. My background in entertainment, user
+            experience curation, and digital media enhances my ability to craft
+            user-friendly websites and applications using HTML5, CSS3, and
+            JavaScript.
+            <br></br>
+            <br></br>I hold a degree in Digital Media: Game Development and have
+            3 years of programming experience. I'm passionate about front-end
+            development, continually expanding my knowledge and proficiency in
+            technologies like React, SASS, Vite, and Git. Moreover, my expertise
+            in design and content creation, utilizing tools like Photoshop,
+            Premiere, Adobe XD, Figma, 3DS Max, and Illustrator, complements my
+            development skills.
             <br></br>
             <br></br>
-            I'm highly aware of the pain points that businesses face when it
-            comes to their website, such as slow load times, unresponsive
-            design, and outdated layouts. I'm dedicated to delivering custom
-            solutions that not only look great but also function seamlessly.
-            <br></br>
-            <br></br>
-            If you're looking for a talented web developer to join your team,
-            let's connect and collaborate. With my passion for creating
-            captivating web experiences and my expertise in front-end web
-            development, I'm confident that I can help take your projects to the
-            next level.
+            As a reliable team player, I'm resourceful, energetic, and curious.
+            I thrive on collaboration and am always eager to explore new ways of
+            working with others to create engaging and meaningful user
+            experiences. Let's discuss how we can leverage our talents to curate
+            exceptional web experiences. Feel free to reach out to me—I look
+            forward to your message.
           </p>
         </div>
       </div>
@@ -49,7 +54,7 @@ const Header = () => {
         />
       </i>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
