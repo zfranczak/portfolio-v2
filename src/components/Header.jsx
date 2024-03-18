f25755320112a914e8b19219f61df74b1a3e6fd3
Zak Franczak <zakfranczak@gmail.com>
2024-03-18 16:20:17 -0500

    changes to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,34 +9,36 @@ const Header = () => {
     <div className='header' id='header'>
       <div className='header-top'>
         <div className='text'>
-          <h1 className='title'>Zachary Franczak</h1>
-          <h2 className='sub-title'>Front End Developer</h2>
           <div className='content'>
-            {/* <img className='headshot' src={headshot} alt='Headshot of Zak' /> */}
-            <p className='p'>
-              Hello! I'm Zachary, a front-end developer with a unique
-              perspective and versatile skill set. My background in
-              entertainment, user experience curation, and digital media
-              enhances my ability to craft user-friendly websites and
-              applications using HTML5, CSS3, and JavaScript.
-              <br />
-              <br />
-              I hold a degree in Digital Media: Game Development and have 3
-              years of programming experience. I'm passionate about front-end
-              development, continually expanding my knowledge and proficiency in
-              technologies like React, SASS, Vite, and Git. Moreover, my
-              expertise in design and content creation, utilizing tools like
-              Photoshop, Premiere, Adobe XD, Figma, 3DS Max, and Illustrator,
-              complements my development skills.
-              <br />
-              <br />
-              As a reliable team player, I'm resourceful, energetic, and
-              curious. I thrive on collaboration and am always eager to explore
-              new ways of working with others to create engaging and meaningful
-              user experiences. Let's discuss how we can leverage our talents to
-              curate exceptional web experiences. Feel free to reach out to me—I
-              look forward to your message.
-            </p>
+            <img className='headshot' src={headshot} alt='Headshot of Zak' />
+            <div className='text-container'>
+              <h1 className='title'>Zachary Franczak</h1>
+              <h2 className='sub-title'>Front End Developer</h2>
+              <p className='p'>
+                Hello! I'm Zachary, a front-end developer with a unique
+                perspective and versatile skill set. My background in
+                entertainment, user experience curation, and digital media
+                enhances my ability to craft user-friendly websites and
+                applications using HTML5, CSS3, and JavaScript.
+                <br />
+                <br />
+                I hold a degree in Digital Media: Game Development and have 3
+                years of programming experience. I'm passionate about front-end
+                development, continually expanding my knowledge and proficiency
+                in technologies like React, SASS, Vite, and Git. Moreover, my
+                expertise in design and content creation, utilizing tools like
+                Photoshop, Premiere, Adobe XD, Figma, 3DS Max, and Illustrator,
+                complements my development skills.
+                <br />
+                <br />
+                As a reliable team player, I'm resourceful, energetic, and
+                curious. I thrive on collaboration and am always eager to
+                explore new ways of working with others to create engaging and
+                meaningful user experiences. Let's discuss how we can leverage
+                our talents to curate exceptional web experiences. Feel free to
+                reach out to me—I look forward to your message.
+              </p>
+            </div>
           </div>
         </div>
       </div>
