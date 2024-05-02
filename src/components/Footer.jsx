a15efa34f15563e6c49491d4ed5e90bbe82296f2
Zak Franczak <zakfranczak@gmail.com>
2024-05-02 10:50:36 -0500

    form updated to syle of page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,16 @@
-import '../styles/Footer.css'
-import { FaReact } from 'react-icons/fa'
+import '../styles/Footer.css';
+import { FaReact } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div className='footer'>
       <a href='https://github.com/zfranczak/portfolio-v2' target='_blank'>
-        <p>Built using React&nbsp;</p>
+        <p>
+          "Programming is not about typing, it's about thinking." - Rich Hickey
+        </p>
       </a>
-      <i>
-        <a href='https://github.com/zfranczak/portfolio-v2' target='_blank'>
-          <FaReact className='react-icon' />
-        </a>
-      </i>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
