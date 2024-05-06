9925dc56215496af3590a22f3d134935aafff933
Zak Franczak <zakfranczak@gmail.com>
2024-05-06 10:42:47 -0500

    img problem

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -4,7 +4,7 @@ export const projectData = [
     id: 'doggie',
     description:
       'Doggie Dilemma is a web app where users can find shelter dogs for adoption. They start by logging in with their name and email, authenticate via the provided login endpoint, and then browse available dogs on a search page. The page allows users to filter dogs by breed, paginate results, and sort alphabetically. ',
-    image: '../assets/doggie-dilemma.jpg',
+    image: '../src/assets/doggie-dilemma1.jpg',
     liveSite: 'https://stately-duckanoo-82a9e1.netlify.app/',
     github: 'https://github.com/zfranczak/fetch-th',
     tags: [
@@ -21,7 +21,7 @@ export const projectData = [
     id: 'watch',
     description:
       "A movie watchlist app, created for a month-long hackathon, featuring new APIs and technologies. It seamlessly integrates various data sources, enhancing the user's movie discovery and tracking experience. This project demonstrates an agile adoption of emerging technologies to craft a functional and user-friendly solution for movie enthusiasts.",
-    image: '../assets/WatchWise.jpg',
+    image: '../src/assets/WatchWise1.jpg',
     liveSite: 'https://chic-semolina-076749.netlify.app/',
     github: 'https://github.com/zfranczak/watchWise',
     tags: [
@@ -40,7 +40,7 @@ export const projectData = [
     id: 'inspire',
     description:
       "A motivational app for an AI-themed hackathon. The app generates motivational quotes from a dataset of famous athletes using ChatGPT's API and pairs them with images using the Unsplash API. My team and I used GitHub version control to streamline development and collaborate. The result was a functional and unique product that showcased our ability to integrate multiple APIs and technologies.",
-    image: '../assets/AI-app.jpg',
+    image: '../src/assets/AI-app1.jpg',
     liveSite: 'https://www.inspirebot3000.com',
     github: 'https://github.com/chat-gpt-hack/Inspire-GPT',
     tags: [
@@ -59,7 +59,7 @@ export const projectData = [
     id: 'pistachio',
     description:
       "A 2D slingshot game developed using Phaser.io and Matter.js. It features advanced physics-based gameplay mechanics, including gravity manipulation and collision detection, resulting in a unique and challenging gaming experience. The game's scalable architecture allows for easy addition of new levels and game elements, making development efficient.",
-    image: '../assets/pistachio-peet.png',
+    image: '../src/assets/pistachio-peet1.png',
     liveSite: 'https://zfranczak.github.io/pistachio-peet/',
     github: 'https://github.com/zfranczak/pistachio-peet',
     tags: ['JavaScript', 'Phaser.io', 'Matter.js', 'Adobe Photoshop', 'Game'],
