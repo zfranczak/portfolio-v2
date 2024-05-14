aaff19230d61400f6b4ec808eb43234ba7a05231
Zak Franczak <zakfranczak@gmail.com>
2024-05-14 11:09:46 -0500

    adding responsive design for smaller screens.

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 function TechStack({ techName, techIcon, link, color }) {
   const IconComponent = techIcon;
-  const iconColor = { color };
+  const iconStyle = { fill: color };
+
   return (
     <div className='tech-unit'>
       <div>
-        <IconComponent className='icons' style={iconColor} />
+        <IconComponent className='icons' style={iconStyle} />
         <p className='tech-name'>{techName}</p>
       </div>
     </div>
